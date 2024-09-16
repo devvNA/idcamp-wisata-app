0427b1a122e8dfc3e02e5e45611851d9b1d8fe41
Devit Nur Azaqi <devitazaqi@gmail.com>
2024-09-16 16:56:54 +0700

    fix:layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:id_camp/detail_screen.dart';
+import 'package:id_camp/main_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const DetailScreen(),
+      home: const MainScreen(),
     );
   }
 }
